96d4376c5bb34ba6a1b155198057b386d11faac0
itay0611 <94786775+itay0611@users.noreply.github.com>
2023-01-10 11:56:33 +0200

    insert into car state v1
    DB adapter classes and implementation of insert_into_car_state

---
+++ timescaleDBAdapter.py
@@ -0,0 +1,62 @@
+import psycopg2
+from DBAdapter import DBAdapter as Adapter
+from overrides import override
+
+class timescaleDBAdapter(Adapter):
+    def __init__(self):
+        # super.__init__()
+        self.connection_string = "postgres://postgres:password@localhost:5432/formulaDB"
+        self.conn = psycopg2.connect(self.connection_string)
+        self.cursor = self.conn.cursor()
+        self.car_state_num = 0
+
+    @override(Adapter)
+    def insert_into_car_state(self, params):
+        """
+        function inserts values into the car_state table
+        :param params: a dictionary of values. Should contain:
+            position vector (x,y),
+            position deviation vector (x,y),
+            velocity vector (x,y),
+            velocity deviation vector (x,y),
+            theta,
+            theta deviation,
+            theta dot,
+            theta dot deviation,
+            steering angle,
+            steering angle deviation,
+            acceleration,
+            acceleration deviation
+        :return:
+        """
+        insert_into_car_state_query = """
+        INSERT INTO car_state VALUES ({state_num}, {position_x}, {position_y}, {position_deviation_x}, {position_deviation_y},
+        {velocity_x}, {velocity_y}, {velocity_deviation_x}, {velocity_deviation_y}, {theta}, {theta_deviation}, {theta_dot}, 
+        {theta_dot_deviation}, {steering_angle}, {steering_angle_deviation}, {acceleration}, {acceleration_deviation})
+        """.format(
+            state_num=self.car_state_num,
+            position_x=params["position_vector"][0],
+            position_y=params["position_vector"][1],
+            position_deviation_x=params["position_deviation_vector"][0],
+            position_deviation_y=params["position_deviation_vector"][1],
+            velocity_x=params["velocity_vector"][0],
+            velocity_y=params["velocity_vector"][1],
+            velocity_deviation_x=params["velocity_deviation_vector"][0],
+            velocity_deviation_y=params["velocity_deviation_vector"][1],
+            theta=params["theta"],
+            theta_deviation=params["theta_deviation"],
+            theta_dot=params["theta_dot"],
+            theta_dot_deviation=params["theta_dot_deviation"],
+            steering_angle=params["steering_angle"],
+            steering_angle_deviation=params["steering_angle_deviation"],
+            acceleration=params["acceleration"],
+            acceleration_deviation=params["acceleration_deviation"]
+        )
+
+        try:
+            self.cursor.execute(insert_into_car_state_query)
+            self.car_state_num += 1
+        except (Exception, psycopg2) as e:
+            print(e)
+
+        self.conn.commit()  # TODO: Do we need it now?
