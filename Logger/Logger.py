b15ab8c3aa411cb25989770d6cdb88d34b2b5220
yosef <yosef>
2023-01-22 18:53:23 +0200

    refactored Logger experiment and experiment managment

--- Logger/Logger.py
+++ Logger/Logger.py
@@ -10,29 +10,50 @@ class Logger:
     messages = []
     channel = None
     stub = None
+    simid = None
+
+    class ExpectedOpenConnection(Exception):
+        def __init__(self):
+            super.__init__("expected an to have an open connection here.")
     
     @classmethod
-    def connect(cls):
-        cls.channel = grpc.insecure_channel('localhost:50051')
-        cls.stub = fservice_pb2_grpc.MessagePassingStub(channel)
-    
+    def connect(cls, dst_url = 'localhost:50051'):
+        try:
+            cls.channel = grpc.insecure_channel(dst_url)
+        except TypeError as e:
+            trailer = ""
+            if hasattr(e, 'message'):
+                trailer = " error message: '"+e.message+"'"
+            raise Exception("failed to connect to destination server." + trailer)
+        cls.stub = fservice_pb2_grpc.MessagePassingStub(cls.channel)
     
     @classmethod
     def newExperiment(cls, simid):
+        if cls.simid is not None:
+            raise Exception("newExperiment called before current experiment had ended.")
+
+        cls.simd = simid
         startMsg = messages.SimulationStartRequest()
         startMsg.simulationid = simid
+        if cls.stub is None:
+            raise Logger.ExpectedOpenConnection()
         ack = cls.stub.SimulationStart(startMsg)
         cls.messages.append(ack.ackmessage)
 
     @classmethod
-    def endExperiment(cls, simid):
+    def endExperiment(cls):
         endMsg = messages.NotifySimulationEnd()
-        endMsg.simulationid = simid
+        endMsg.simulationid = cls.simid
+        if cls.stub is None:
+            raise Logger.ExpectedOpenConnection()
         ack = cls.stub.SimulationStart(endMsg)
         cls.messages.append(ack.ackmessage)
+        cls.simid = None
 
     @classmethod
     def log(cls, message):
+        if cls.stub is None:
+            raise Logger.ExpectedOpenConnection()
         ack = cls.stub.GetMessage(message)
         cls.messages.append(ack.ackmessage)
 
--- Logger/Test.py
+++ Logger/Test.py
@@ -2,10 +2,15 @@ from Control import Control
 from StateEstimation import StateEstimation
 from Perception import Perception
 from Logger import Logger
+from random import randint
+
 def main():
     
     Logger.connect()
     
+    id = randint(0, 10000)
+    Logger.newExperiment(id)
+    print(f"createing experiment {id=}")
     for _ in range(3):
         Control.send_message()
 
@@ -17,7 +22,8 @@ def main():
     
     for _ in range(3):
         Perception.send_message(True)
-        
+    Logger.endExperiment()
+
     return
 
 
